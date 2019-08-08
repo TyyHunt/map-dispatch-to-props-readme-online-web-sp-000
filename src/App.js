22977b3812596dfbe73283b9781164add53ad94c
Tyler Hunt <tyhunt04@gmail.com>
2019-08-08 01:06:31 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { addItem } from  './actions/items';
 class App extends Component {
 
   handleOnClick() {
-    this.props.store.dispatch(addItem());
+    this.props.addItem();
   }
 
   render() {
@@ -26,5 +26,13 @@ const mapStateToProps = (state) => {
     items: state.items
   };
 };
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addItem: () => {
+      dispatch(addItem())
+    }
+  }
+}
 
 export default connect(mapStateToProps)(App);
